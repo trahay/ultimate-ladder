d139d66b975db8d1dbeb621cbf4c96214b471e1f
François Trahay <francois.trahay@telecom-sudparis.eu>
2024-01-04 23:12:21 +0100

    init script

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ source venv/bin/activate
 git submodule init
 git submodule update
 
-pip install django, pandas
+pip install django pandas
 
 python manage.py makemigrations ladder
+python manage.py migrate ladder
+python manage.py migrate
